d046443969e58b17f3b92485e93e17657653a6fc
ridwannhd0705 <ridwannur07@student.uns.ac.id>
2025-03-05 01:09:59 +0700

    set redirect logout login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -19,7 +19,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View
     {
-        return view('auth.login');
+        return view('auth.login-bpkp');
     }
 
     /**
@@ -72,6 +72,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect('/login');
     }
 }
